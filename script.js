1e68aa49635a59be8d713fce76ebc84d5c48b84a
rohanduggal3g74 <rohanduggal3g74@gmail.com>
2024-09-04 18:01:03 +0530

    project completed full

--- script.js
+++ script.js
@@ -30,16 +30,60 @@ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 ScrollTrigger.refresh();
 
 }
-locomotiveanimation()
+
 function curserAnimation(){
-    document.addEventListener("mousemove",function(dets){
-    gsap.to("#crsr",{
-        left:dets.x,
-        top:dets.y
+    Shery.mouseFollower({
+        //Parameters are optional.
+        skew: true,
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 1,
+      });
+Shery.makeMagnet("#nav-part2 h4", { });
+var videocontainerr=document.querySelector("#video-container");
+var video=document.querySelector("#video-container video")
+videocontainerr.addEventListener("mouseenter",function(){
+    videocontainerr.addEventListener("mousemove",function(dets){
+        gsap.to(".mousefollower",{
+            opacity:0
+        })
+        gsap.to("#video-cursor",{
+            left:dets.x -500,
+            y:dets.y-300
+        })
     })
 })
+videocontainerr.addEventListener("mouseleave",function(){
+    gsap.to(".mousefollower",{
+        display:"intial",
+        opacity:1,
+    })
+    gsap.to("#video-cursor",{
+        left:"70%",
+        top:"-15%"
+    })
+})
+var flag=0
+videocontainerr.addEventListener("click",function(){
+    if(flag==0){
+    video.play()
+    video.style.opacity=1
+    document.querySelector("#video-cursor").innerHTML=`<i class="ri-pause-mini-line"></i>`
+    gsap.to("#video-cursor",{
+        scale:0.5
+    })
+    flag=1
+}else{
+    video.pause()
+    video.style.opacity=0
+    document.querySelector("#video-cursor").innerHTML=`<i class="ri-play-mini-line"></i>`
+    gsap.to("#video-cursor",{
+        scale:1
+    })
+    flag=0
 }
-// curserAnimation()
+})
+}
+
 function loadingAnimation(){
     var tl=gsap.timeline()
     tl.from(".line h1", {
@@ -71,7 +115,7 @@ function loadingAnimation(){
     tl.to("#loader",{
         opacity:0,
         duration:0.2,
-        delay:0
+        delay:2
     });
     tl.from("#page1",{
         delay:0.2,
@@ -95,7 +139,61 @@ function loadingAnimation(){
         opacity:0,
     },"-=1.2");
 }
+
+
+function sheryAnimation(){
+    Shery.imageEffect(".image-div", {
+        style: 5,
+        config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7333206846226437},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.46,"range":[0,10]},"metaball":{"value":0.5,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.44,"range":[0,2]},"noise_scale":{"value":7.44,"range":[0,100]}},
+        gooey:true
+      });
+}
+function flaganimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#flag",{
+            x:dets.x,
+            y:dets.y
+        })
+    })
+    document.querySelector("#hero3").addEventListener("mouseenter",function(){
+        gsap.to("#flag",{
+            opacity:1
+        })
+    })
+    document.querySelector("#hero3").addEventListener("mouseleave",function(){
+        gsap.to("#flag",{
+            opacity:0
+        })
+    })
+}
+function footeranimation(){
+    var footerh1=document.querySelector("#footer h1");
+    footerh1.addEventListener("mouseover",function(){
+        gsap.from("#footer h1",{
+            opacity:0,
+            y:50,
+            delay:0.5,
+            duration:1,
+            onStart:function(){
+                $("#footer h1").textillate({ in: { effect: 'rollIn' } });
+            }
+        })
+    })
+    footerh1.addEventListener("mouseleave",function(){
+        gsap.to("#footer h1",{
+            display:"intial",
+            opacity:1
+
+        })
+    })
+    
+}
 loadingAnimation()
-Shery.makeMagnet("#nav-part2 h4", { });
+curserAnimation()
+locomotiveanimation()
+flaganimation()
+sheryAnimation()
+// footeranimation()
+
 
 
